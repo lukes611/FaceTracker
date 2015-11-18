8bf1f8df7e67a7181ec9a045a20ab8bec291e08a
luke lincoln <lukes611@gmail.com>
2015-11-19 07:10:52 +1000

    making resources local

--- LFaceDetect.py
+++ LFaceDetect.py
@@ -1,13 +1,16 @@
 import numpy as np
 import cv2
 
+#Face detector object
 class LukesFaceDetector:
 	def __init__(self):
-		self.fileLocation = 'C:/Users/luke/Documents/ocv3a/ocvbinaries/install/etc/haarcascades/'
+		#initialize resources
+		self.fileLocation = 'resources/'
 		self.faceCascade = cv2.CascadeClassifier(self.fileLocation+'haarcascade_frontalface_default.xml')
 		self.eyeCascade = cv2.CascadeClassifier(self.fileLocation+'haarcascade_eye.xml')
 		self.faces = []
 		self.eyes = []
+	#detect faces and eyes
 	def detect(self, im):
 		gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 		self.faces = self.faceCascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30, 30),flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
@@ -19,12 +22,14 @@ class LukesFaceDetector:
 				eyes[i][0] += x
 				eyes[i][1] += y
 			self.eyes.extend(eyes)
-	
+	#draw a rectangle
 	def rect(self, img, x, y, w, h, col):
 		cv2.rectangle(img,(x,y),(x+w,y+h),col,2)
+	#draw faces on img
 	def drawFaces(self, img):
 		for x,y,w,h in self.faces:
 			self.rect(img, x, y, w, h, (255, 0, 0))
+	#draw eyes on img
 	def drawEyes(self, img):
 		for x,y,w,h in self.eyes:
 			self.rect(img, x, y, w, h, (0, 255, 0))
--- main.py
+++ main.py
@@ -2,21 +2,31 @@ import numpy as np
 import cv2
 from LFaceDetect import LukesFaceDetector
 
+#init video capture object
 cap = cv2.VideoCapture(0)
 
+#create face detection object
 faceDetector = LukesFaceDetector()
 
+
 while True:
-	ret, frame = cap.read()
+	#whilst user hasn't quit:
 	
+	#read frame
+	ret, frame = cap.read()
+
+	#detect faces
 	faceDetector.detect(frame)
 	
+	#mark image with rectangles around faces and eyes
 	faceDetector.drawFaces(frame)
 	faceDetector.drawEyes(frame)
 	
+	#show user the augmented image
 	cv2.imshow('frame', frame)
 	if cv2.waitKey(30) == 27:
 		break
-	
+
+#free resources		
 cap.release()
 cv2.destroyAllWindows()
